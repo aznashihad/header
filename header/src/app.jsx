55a4d7af9de1c7919455039ff747af9c2e57d7e4
aznashihad <155214077+aznashihad@users.noreply.github.com>
2024-06-21 09:14:49 +0400

    'header'

---
+++ header/src/app.jsx
@@ -0,0 +1,15 @@
+import "./APP.css";
+import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
+
+function App(){
+    //react fragment
+    return(
+        <>
+        <Header/>
+        <Footer/>
+        <Banner/>
+        </>
+    )
+}
+export default App
---
+++ header/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+function Header() {
+    return (
+        <div className="header-wrapper">
+            <h2>logo</h2>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>support</li>
+                <li>contact</li>
+            </ul>
+        </div>
+    );
+
+}
+export default Header;
